57e8680fe318c137bd26f0771aa58ad7710e9165
Nour_mohamed <148482343+NourMohamed6@users.noreply.github.com>
2024-05-18 03:38:55 +0300

    Update Item.h

--- Item.h
+++ Item.h
@@ -4,22 +4,20 @@
 #include "iostream"
 
 using namespace std;
-
-class Item {
+class Item
+{
     string itemName;
     string category;
     int price;
 public:
     Item();
-
     string ItemName() { return this->itemName; };
     string Category() { return this->category; };
     int Price() { return this->price; };
-
     Item(string itemName, string category, int price);
-
-    bool operator<(const Item &item) const;
-
+    bool operator>(const Item& item) const;
+    bool operator<(const Item& item) const;
+    bool operator==(const Item& item) const;
     void print();
 };
 
