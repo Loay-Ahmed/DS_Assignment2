4ac572109f06e98f8109a6d756e1017150751534
Nour_mohamed <148482343+NourMohamed6@users.noreply.github.com>
2024-05-18 03:40:12 +0300

    Update BST.h

--- BST.h
+++ BST.h
@@ -1,24 +1,36 @@
 #ifndef DS_ASSIGNMENT2_BST_H
 #define DS_ASSIGNMENT2_BST_H
 
-#include "Item.h"
-#include "Tree.h"
+#include"Item.h"
+#include<string>
 
-class BST : Tree {
+
+class BST
+{
 private:
-    struct Node {
-        Item item;
-        Node* left;
-        Node* right;
-    };
-    Node* root;
-public:
-    BST();
-    void inOrder(Node* node);
-    void postOrder(Node* node);
-    void insertNode(const string& itemName, const string& category, int price) override;
-    void deleteNode(const string& name) ;
-};
+	struct Node {
+		Item item;
+		Node* left;
+		Node* right;
+	};
+	Node* root;
 
+
+	void deletC(Node* prev, Node* node, string itm);
+
+public:
+	Node*& getRoot();
+	BST();
+	void deleteNode(string DI);
+	void delet(Node* prev, Node* node);
+	Item findMin(Node* node);
+	void printInOrder(Node* nude);
+	void printInOrderDES(Node* node);
+	void insertNode(const string& itemName, const string& category, int price);
+	void insert(Node*& node, const Item& itm);
+	void insertByPrice(Node*& node, const Item& itm);
+	void printPreOrder(Node* node);
+	void copy(Node*& root, Node* node);
+};
 
 #endif //DS_ASSIGNMENT2_BST_H
